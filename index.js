7bffd26609733ac851abeb636f872390dc0c45b4
Moises <mcastillo093@gmail.com>
2021-07-03 10:15:20 -0600

    auth endpoints setup

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ require('dotenv').config();
 
 
 //creating express server
-
 const app = express();
 
 //Public directory
 app.use( express.static('public'));
 
+// Routes
+app.use('/api/auth', require('./routes/auth'))
+
 
 // listening requests
-
 app.listen( process.env.PORT, () => {
     console.log('server running in port 4000')
 });
---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+/*
+    AUTH ROUTES
+    host + /api/auth
+*/
+
+const { Router } = require('express');
+
+const router = Router();
+
+const { createUser, loginUser, renewToken } = require('../controllers/auth')
+
+router.post('/new', createUser );
+
+router.post('/', loginUser );
+
+router.get('/renew', renewToken );
+
+module.exports = router;
