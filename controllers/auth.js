ca25ac1717b52c195812c0207d24f947f88bf859
Moises <mcastillo093@gmail.com>
2021-07-03 22:10:36 -0600

    passwords encrypted with bcryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 //This is to active the intellisense
-const User = require('../models/user')
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
 
 
 const createUser = async( req, res = response ) => {
@@ -19,6 +20,10 @@ const createUser = async( req, res = response ) => {
         }
 
         user = new User( req.body );
+
+        // Encrypt password
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync( password, salt );
     
         await user.save();
     
@@ -39,16 +44,49 @@ const createUser = async( req, res = response ) => {
 
 }
 
-const loginUser = ( req, res = response ) => {
+const loginUser = async( req, res = response ) => {
 
     const { email, password } = req.body;
 
-    res.status(200).json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-    })
+    try {
+
+        const user = await User.findOne({ email });
+
+        if ( !user ) {
+            return res.status( 400 ).json({
+                ok: false,
+                msg: 'Your email is not registered'
+            })
+        }
+
+        // Validate passwords
+
+        const validPass = bcrypt.compareSync( password, user.password );
+
+        if ( !validPass ) {
+            return res.status( 400 ).json({
+                ok: false,
+                msg: 'Your credentials are incorrect. Please try again!'
+            })
+        }
+
+        // JWT
+
+        res.json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        })
+
+    } catch ( error ) {
+
+        console.log( error )
+        res.status(500).json({
+            ok: false,
+            msg: 'Please contact the administrator'
+        })
+    }
+
 }
 
 const renewToken = ( req, res = responsees ) => {
