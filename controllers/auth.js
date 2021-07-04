65b239ec25c8a6d92b87d299eccee2eb9627862d
Moises <mcastillo093@gmail.com>
2021-07-03 20:28:53 -0600

    validate user in db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,42 @@
 const { response } = require('express');
 //This is to active the intellisense
+const User = require('../models/user')
 
-const createUser = ( req, res = response ) => {
 
-    const { name, email, password } = req.body;
+const createUser = async( req, res = response ) => {
 
-    res.status(201).json({
-        ok: true,
-        msg: 'register',
-        name,
-        email,
-        password
-    })
+    const { email, password } = req.body;
+
+    try {
+
+        let user = await User.findOne({ email });
+
+        if ( user ) {
+            return res.status( 400 ).json({
+                ok: false,
+                msg: 'Your email is already registered'
+            })
+        }
+
+        user = new User( req.body );
+    
+        await user.save();
+    
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        })
+
+    } catch ( error ) {
+
+        console.log( error )
+        res.status(500).json({
+            ok: false,
+            msg: 'Please contact the administrator'
+        })
+    }
+
 }
 
 const loginUser = ( req, res = response ) => {
