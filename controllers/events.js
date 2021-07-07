e93572a5e1a93d7c1f4639b798d38c4e965ab46d
Moises <mcastillo093@gmail.com>
2021-07-06 19:04:36 -0600

    delete event in db

--- controllers/events.js
+++ controllers/events.js
@@ -48,7 +48,7 @@ const updateEvent = async( req, res = response) => {
         const event = await Event.findById( eventId );
 
         if ( !event ) {
-            res.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: 'Event does not exist'
             })
@@ -85,11 +85,41 @@ const updateEvent = async( req, res = response) => {
 
 const deleteEvent = async( req, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'deleteEvent'
-    })
+    const eventId = req.params.id;
+    const uid = req.uid;
 
+    try {
+
+        const event = await Event.findById( eventId );
+
+        if ( !event ) {
+            res.status(404).json({
+                ok: false,
+                msg: 'Event does not exist'
+            })
+        }
+
+        if ( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'You are not allowed to delete this event'
+            })
+        }
+
+        await Event.findByIdAndDelete( eventId );
+
+        res.json({
+            ok: true
+        })
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Contact the administrator'
+        })
+    }
 }
 
 module.exports = {
